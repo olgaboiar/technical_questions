cffaad58037fcad7ae04b978883788575d6f10d6
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-06-11 23:00:37 -0400

    updated the second question

--- 2.rb
+++ 2.rb
@@ -1,40 +1,39 @@
 def decodeString(s)
-    s = s.split("")
     j = 0
-    t = ""
     stack = []
     result = ""
+    # Looping through every character of the string
     while j < s.length
-       character = s[j]
-       if character != "]"
-        stack.push(character)
-       else
-         # read the string from the stack
-        group = ""
-        k = stack.pop
-        while k != "["
-            group = k + group
+        character = s[j]
+        # Pushing every character to the stack until encountering a closing bracket
+        if character != "]"
+            stack.push(character)
+        else
+            # When a closing bracker encountered, get characters from the stack to form a group to be decoded
+            group = ""
             k = stack.pop
+            # Characters are added to the group till opening bracket
+            while k != "["
+                group = k + group
+                k = stack.pop
+            end
+            group = group + result
+            # Finding the numbers in stack
+            num = ""
+            begin
+                k = stack.pop
+                num = k + num
+                last_element = stack.last
+            end until last_element.to_i.to_s != last_element
+            # Decoding the group by adding it num times
+            string = ""
+            num = num.to_i
+            1.upto(num) {string = group + string}
+            stack.push(string)
+
         end
-        group = group + result
-        puts group
-        # read the number from the stack
-        num = ""
-        
-        begin
-            k = stack.pop
-            num = k + num
-            last_element = stack.last
-        end until last_element.to_i.to_s != last_element
-        
-        string = ""
-        num = num.to_i
-        1.upto(num) {string = group + string}
-        result = string
-    
-       end
-       j += 1
+        j += 1
     end
-    puts "result is " + result
+    return stack.join("")
 end
 decodeString(s)
